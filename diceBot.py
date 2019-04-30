15b116e9328a3d6964ef97a2737636e7499df174
ankluz <agent00go@gmail.com>
2019-04-30 12:31:06 +0300

    Изменена структура хранения данных с текстовых документов на базу данных

--- diceBot.py
+++ diceBot.py
@@ -2,61 +2,86 @@ import vk_api
 import requests
 import connect
 import logic
+import sqlite3
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-
-
+Cn = sqlite3.connect('Drop.db')
+curs = Cn.cursor()
 vk_session = vk_api.VkApi(token=connect.token)
 vk = vk_session.get_api()
 
 # получение списка администраторов и забаненых из текстовых файлов
 admins = []
 banlist = []
-with open('admins.txt') as f:
-    admins = f.read().splitlines()
+
+curs.execute('SELECT Vkid FROM Admins')
+row = curs.fetchone()
+while row is not None:
+    admins.append(row[0])
+    row = curs.fetchone()
 print("admins")
 print(admins)
 
-with open('banlist.txt') as f:
-    banlist = f.read().splitlines()
+curs.execute('SELECT Vkid FROM Banlist')
+row = curs.fetchone()
+while row is not None:
+    banlist.append(row[0])
+    row = curs.fetchone()
 print("banned")
 print(banlist)
-
-
+curs.close()
+Cn.close()
 # бесконечный цикл в котором происходит постоянное чтение сообщений в беседах
 longpoll = VkBotLongPoll(vk_session, connect.id)
+i=0
+while i == 0:
+    for event in longpoll.listen():
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            temp = event.obj.text
+            temp = temp.replace(connect.strdel,'')
+            temp = temp.replace(" ","")
+            temp = temp.lower()
+            if temp[:8]=='diceroll':
+                if event.obj.user_id in banlist:
+                    vk.messages.send(
+                        user_id=event.obj.user_id,
+                        peer_id = event.obj.peer_id,
+                        random_id=event.obj.random_id,
+                        message="вы забанены")
+                else:
+                    try:
+                        n=int(temp[8:])
+                    except:
+                        n=20
+                    vk.messages.send(
+                        user_id=event.obj.user_id,
+                        peer_id = event.obj.peer_id,
+                        random_id=event.obj.random_id,
+                        message=logic.roll(n))
 
-for event in longpoll.listen():
-    if event.type == VkBotEventType.MESSAGE_NEW:
-        temp = event.obj.text
-        temp = temp.replace(connect.strdel,'')
-        temp = temp.replace(" ","")
-        temp = temp.lower()
-        if temp[:4]=='roll':
-            if event.obj.user_id in banlist:
-                vk.messages.send(
-                    user_id=event.obj.user_id,
-                    peer_id = event.obj.peer_id,
-                    random_id=event.obj.random_id,
-                    message="вы забанены")
-            else:
-                try:
-                    n=int(temp[4:])
-                except:
-                    n=20
-                vk.messages.send(
-                    user_id=event.obj.user_id,
-                    peer_id = event.obj.peer_id,
-                    random_id=event.obj.random_id,
-                    message=logic.roll(n))
-        elif temp[:3] == 'ban':
-            if event.obj.user_id in admins:
-                logic.ban(temp[3:])
-                with open('banlist.txt') as f:
-                    banlist = f.read().splitlines()
-                print("banned")
-                print(banlist)
-                vk.messages.send(
-                    user_id=event.obj.user_id,
-                    peer_id = event.obj.peer_id,
-                    random_id=event.obj.random_id,
-                    message="пользователь забанен")
+
+            elif temp[:7] == 'diceban':
+                if event.obj.user_id in admins:
+                    banlist = logic.ban(temp[7:],banlist)
+                    print("banned")
+                    print(banlist)
+                    vk.messages.send(
+                        user_id=event.obj.user_id,
+                        peer_id = event.obj.peer_id,
+                        random_id=event.obj.random_id,
+                        message="пользователь забанен")
+
+
+            elif temp[:8] == 'diceжанр':
+                if event.obj.user_id in banlist:
+                    vk.messages.send(
+                        user_id=event.obj.user_id,
+                        peer_id = event.obj.peer_id,
+                        random_id=event.obj.random_id,
+                        message="вы забанены")
+                else:
+                    row = logic.style()
+                    vk.messages.send(
+                        user_id = event.obj.user_id,
+                        peer_id = event.obj.peer_id,
+                        random_id = event.obj.random_id,
+                        message = "Жанр: %s \nОписание: %s" % (row[1],row[2]))
--- logic.py
+++ logic.py
@@ -2,31 +2,39 @@ import vk_api
 import random
 import configobj
 import connect
-
+import sqlite3
 
 
 def roll(n):
         m = '%i/%i' % (random.randint(1,n),n)
         return m
 
-def adminAdd( id, list):
-    with open('admins.txt',"a") as f:
-        f.write(id+"\n")
-    list.append(id)
-    print("admin add\n")
 
 def ban(id,list):
-    with open('banlist.txt',"a") as f:
-        f.write(id+"\n")
+    cn = sqlite3.connect('Drop.db')
+    curs = cn.cursor()
+    curs.execute("INSERT INTO Banlist (Vkid) VALUES ('%i')" % (id))
+    cn.commit()
     list.append(id)
     print("banned\n")
+    curs.close()
+    cn.close()
+    return list
 
-def unban(id):
-    f = open("banlist.txt","r")
-    temp = f.readlines()
-    f.close()
-    f = open("banlist.txt", "w")
-    for line in temp:
-        if line != id + "\n":
-            f.write(line)
-    f.close()
+def unban(id,list):
+    cn = sqlite3.connect('Drop.db')
+    curs = cn.cursor()
+    curs.execute("DELETE FROM Banlist (Vkid) WHERE Vkid='%i'" % (id))
+    cn.commit()
+    curs.close()
+    cn.close()
+    list.remove(id)
+
+def style():
+    cn = sqlite3.connect('Drop.db')
+    curs = cn.cursor()
+    curs.execute("SELECT * FROM Styles WHERE Id = '%i'" % (random.randint(1,40)))
+    row = curs.fetchone()
+    curs.close()
+    cn.close()
+    return row
